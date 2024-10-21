5f6cb0d8ef61ff959403c2c9e39e5bf3ad06cee4
Stathis Iakovidis <iakovidis@entranet.gr>
2024-10-21 19:54:11 +0200

    production property in config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProduction ? "/personal-portfolio-next" : "",
   reactStrictMode: true,
   output: "export",
   distDir: "dist",
